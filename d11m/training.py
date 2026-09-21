860694885deb86fa0a1fb3647ddbb867eb98d017
rczajkadev <rafalcz29@gmail.com>
2026-09-21 17:05:20 +0200

    Add gradient clipping to training

--- d11m/training.py
+++ d11m/training.py
@@ -58,6 +58,12 @@ def train_gen(
             )
 
             loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(
+                model.parameters(),
+                max_norm=1.0
+            )
+
             optimizer.step()
 
             batch_tokens = target_tokens.numel()
