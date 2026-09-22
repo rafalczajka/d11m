9ee72d3f1e30eb3b329d252dc62fcbfd851f05fb
rczajkadev <rafalcz29@gmail.com>
2026-09-22 22:48:28 +0200

    Replace argmax with softmax

--- d11m/generation.py
+++ d11m/generation.py
@@ -1,7 +1,28 @@
 import torch
+from torch import Tensor
 
 from . import tokenizer
 from .model import Model
+
+SOFTMAX_TEMPERATURE = 0.8
+
+
+def _get_next_token(
+    logits: Tensor,
+    temperature: float,
+) -> Tensor:
+    if temperature <= 0:
+        raise ValueError("Temperature must be greater than 0")
+
+    probabilities = torch.softmax(
+        logits / temperature,
+        dim=-1
+    )
+
+    return torch.multinomial(
+        probabilities,
+        num_samples=1
+    ).squeeze(0)
 
 
 def generate(
@@ -33,9 +54,11 @@ def generate(
             input_tokens = input_tokens.unsqueeze(0)
 
             logits = model(input_tokens)
+
             last_token_logits = logits[0, -1]
             last_token_logits[ignored_tokens] = float('-inf')
-            next_token = torch.argmax(last_token_logits) # TODO: use softmax
+
+            next_token = _get_next_token(last_token_logits, SOFTMAX_TEMPERATURE)
 
             if next_token.item() == tokenizer.EOS:
                 break
